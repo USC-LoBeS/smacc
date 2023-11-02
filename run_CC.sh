99c67f0d4f4ac434d94e9af64927b8ec6579ae00
ShrutiGadewar <39843804+ShrutiGadewar@users.noreply.github.com>
2023-11-02 14:30:34 -0700

    Add files via upload

--- run_CC.sh
+++ run_CC.sh
@@ -2,23 +2,23 @@
 #$ -S /bin/bash
 
 # Export python environment
-export PATH=./Anaconda3/envs/smacc/bin:$PATH
+export PATH=/ifs/loni/faculty/njahansh/nerds/shruti/software/python/anaconda3/envs/smacc/bin:$PATH
 
+SCRIPT_DIR="$(pwd)"
 # Input directory of T1
-dirI=./input
+dirI=${SCRIPT_DIR}/input
 
 # Subject list for T1's to be processed
-dirS=./subject_list.txt
+dirS=${SCRIPT_DIR}/subject_list.txt
 
 # Model folder path where the weights for the UNet and AutoQC model is stored
-dirM=./model
+dirM=${SCRIPT_DIR}/model
 
 # Output directory 
-dirO=./output
+dirO=${SCRIPT_DIR}/output
 mkdir -p ${dirO}
 mkdir -p ${dirO}/segmentation
 mkdir -p ${dirO}/metrics
-mkdir -p ${dirO}/QC
 
 # Modality of the image (T1/T2/FLAIR)
 modality=T1
@@ -26,7 +26,7 @@ modality=T1
 #########################################################################################################################
 ########## DO NOT CHANGE THIS PATH ########## 
 # Python Script path 
-script=./scripts
+script=${SCRIPT_DIR}/scripts
 cd ${script}
 
 #########################################################################################################################
@@ -37,7 +37,13 @@ python generate_segmentations.py --inp ${dirI} --model_path ${dirM} --out ${dirO
 # Step 2: Extract and Collate CC metrics------->
 for entry in "${dirO}/segmentation/nifti"/*
 do
-	python extract_metrics.py --mask ${entry} --output ${dirO}/metrics/${entry}.csv
+	IFS='/'
+	read -ra newarr <<< "$entry"
+	subj=${newarr[-1]}
+	savename=${subj::-7}
+	IFS=' '
+
+	python extract_metrics.py --mask ${entry} --output ${dirO}/metrics/${savename}.csv
 	echo "Done with ${entry}"
 done
 python collate_metrics.py --inp ${dirO}/metrics --out ${dirO}
